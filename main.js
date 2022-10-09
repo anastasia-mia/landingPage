69d8d9eaa3be3fb2160b9df7644a93d5409552b0
anastasia_mia <nastia.mianovska333@gmail.com>
2022-10-09 15:49:14 +0300

    header adaptation

---
+++ main.js
@@ -0,0 +1,8 @@
+let burger = document.querySelector('.header_burger');
+let menu = document.querySelector('.header_menu');
+burger.addEventListener('click', function (){
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+    document.body.classList.toggle('lock');
+
+})
