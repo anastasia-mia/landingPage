fac8b083e396f0fffd26b680fafb89e72aa55d0b
anastasia_mia <nastia.mianovska333@gmail.com>
2023-02-05 10:18:37 +0200

    slider refactoring

--- js/slider.js
+++ js/slider.js
@@ -1,60 +1,77 @@
-let blocks = document.querySelectorAll('.subscription_block');
-let singleBlockWidth = document.querySelector('.subscription_block').offsetWidth;
-let container = document.querySelector('.subscription_container');
-let blockAndGapWidth = (container.offsetWidth - (singleBlockWidth * blocks.length)) / (blocks.length - 1) + singleBlockWidth;
-let switches = document.querySelectorAll('.subscription_switch');
-if(window.innerWidth <= 905){
-    blocks[0].classList.add('active');
-    switches[0].style.backgroundColor = '#043873';
-    let num = 0;
-    let start = 0;
-    blocks.forEach(function(e){
-        e.addEventListener('click', () =>{
-            if(num !== e.dataset.index){
-                blocks.forEach(block => {
-                    block.classList.remove('active');
-                })
-                if(e.dataset.index < num){
-                    start += blockAndGapWidth;
-                    container.style.transform = 'translate(' + start +'px)';
-                }
-                else if(e.dataset.index > num){
-                    start -= blockAndGapWidth;
-                    container.style.transform = 'translate(' + start + 'px)';
-                }
-                num = e.dataset.index;
-                e.classList.add('active');
-                switches[e.dataset.index].style.backgroundColor = '#043873';
-            }
-        })
-    })
+function getWidth (nameClass, target, container){
+    let blocks = target .querySelectorAll('.' + nameClass + '_block');
+    let singleBlockWidth = target.querySelector('.' + nameClass + '_block').offsetWidth;
+    let blockGapWidth = (container.offsetWidth - (singleBlockWidth * blocks.length)) / (blocks.length - 1) + singleBlockWidth;
+    let elementParameters = {
+        blocks: Array.from(blocks),
+        container: container,
+        blockAndGapWidth: blockGapWidth
+    };
+    return elementParameters;
 }
 
-let point = 0;
-let start = 0;
+let sliders = document.querySelectorAll('.slider');
+let switches = document.querySelectorAll('.switches');
+if(window.innerWidth <= 905){
+    document.querySelector('.subscription_block').classList.add('active');
+    document.querySelector('.reviews_block').classList.add('active');
+}
+document.querySelector('.subscription_switch').style.backgroundColor = '#043873';
+document.querySelector('.reviews_switch').style.backgroundColor = '#043873';
+sliders.forEach((e) =>{
+    let num = 0;
+    let start = 0;
+    e.addEventListener('click', (ev) =>{
+        let nameClass = e.classList[1];
+        let container = e.querySelector('.' + nameClass + '_container');
+        let element = getWidth(nameClass, e, container);
+        if(window.innerWidth <= 905 && container.offsetWidth > window.innerWidth){
+            let target = ev.target.closest('.' + nameClass + '_block');
+            if(num !== target.dataset.index){
+                element.blocks.forEach(block => {
+                    block.classList.remove('active');
+                })
+                if(target.dataset.index < num){
+                    start += element.blockAndGapWidth;
+                    element.container.style.transform = 'translate(' + start +'px)';
+                }
+                else if(target.dataset.index > num){
+                    start -= element.blockAndGapWidth;
+                    element.container.style.transform = 'translate(' + start + 'px)';
+                }
+                num = target.dataset.index;
+                target.classList.add('active');
+            }
+    }
+    })
+})
+
 switches.forEach((e) => {
-    e.addEventListener('click', () =>{
-        if(point !== e.dataset.index){
-            switches.forEach( swittch => {
-                swittch.style.background = '#4F9CF9';
-            })
-            blocks.forEach(block => {
-                block.classList.remove('active');
-            });
-            e.style.backgroundColor = '#043873';
-            blocks[e.dataset.number].classList.add('active');
-            let difference = e.dataset.number - point;
-            if(e.dataset.number < point){
-                start += Math.abs(blockAndGapWidth * difference);
+    let point = 0;
+    let start = 0;
+    e.addEventListener('click', (ev) =>{
+        let nameClass = e.classList[0].substring(0, e.classList[0].indexOf('_'));
+        let container = e.previousElementSibling;
+        let slider = document.querySelector('.' + nameClass);
+        let element = getWidth(nameClass, slider, container);
+        let switchesArray = Array.from(e.children)
+        if(point !== ev.target.dataset.number && ev.target.classList.contains('switch')){
+            for( let i in switchesArray){
+                switchesArray[i].style.backgroundColor = '#4F9CF9';
+                element.blocks[i].classList.remove('active');
+            }
+            ev.target.style.backgroundColor = '#043873';
+            element.blocks[ev.target.dataset.number].classList.add('active');
+            let difference = ev.target.dataset.number - point;
+            if(ev.target.dataset.number < point){
+                start += Math.abs(element.blockAndGapWidth * difference);
                 container.style.transform = 'translate(' + start +'px)';
-                console.log(start);
             }
-            else if(e.dataset.number > point){
-                start -= blockAndGapWidth * difference;
+            else if(ev.target.dataset.number > point){
+                start -= element.blockAndGapWidth * difference;
                 container.style.transform = 'translate(' + start + 'px)';
-                console.log(start);
             }
-            point = e.dataset.number;
+            point = ev.target.dataset.number;
         }
     })
 })
