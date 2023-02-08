41bb084419ff5b6d8653dd7e6e16ac69ee2c8562
anastasia_mia <nastia.mianovska333@gmail.com>
2023-02-08 21:49:04 +0200

    the 2 part of footer creating

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ burger.addEventListener('click', function (){
     menu.classList.toggle('active');
     document.body.classList.toggle('lock');
 })
+
+let languageSelection = document.querySelector('.footer_language');
+languageSelection.addEventListener('click', (e) =>{
+    let currentLanguage = e.currentTarget;
+    let chosenLanguage = e.target;
+    console.log(currentLanguage.firstElementChild);
+    if(chosenLanguage.parentNode.tagName === 'UL'){
+        currentLanguage.firstElementChild.innerText = chosenLanguage.innerText;
+    }
+    else{
+        return false;
+    }
+})
